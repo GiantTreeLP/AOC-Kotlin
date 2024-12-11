e34480c112f6575e2ccea49b92186bce36363f38
Marvin <GiantTreeLP@users.noreply.github.com>
2024-12-11 23:00:25 +0100

    Add solutions for day 11. The solution for part 2 also works for part 1. There are not that many distinct numbers to cache, so this works surprisingly well.

---
+++ src/main/kotlin/day11/part2.kt
@@ -0,0 +1,51 @@
+package day11
+
+import common.readResource
+import day11.Part2.countStones
+
+private object Part2 {
+
+    fun applyRule(stone: Long): List<Long> {
+        return buildList {
+            if (stone == 0L) {
+                // Rule 1: 0 -> 1
+                add(1)
+            } else {
+                val stoneString = stone.toString()
+                if (stoneString.length % 2 == 0) {
+                    // Rule 2: 1234 -> 12 34
+                    add(stoneString.substring(0, stoneString.length / 2).toLong())
+                    add(stoneString.substring(stoneString.length / 2).toLong())
+                } else {
+                    // Rule 3: 1 -> 1*2024
+                    add(stone * 2024L)
+                }
+            }
+        }
+    }
+
+    fun countStones(stone: Long, remaining: Int, cache: MutableMap<Pair<Long, Int>, Long>): Long {
+        if (remaining == 0) {
+            return 1
+        }
+
+        val key = Pair(stone, remaining)
+        if (key in cache) {
+            return cache[key]!!
+        }
+
+        val result = applyRule(stone).sumOf { countStones(it, remaining - 1, cache) }
+        cache[key] = result
+        return result
+    }
+
+}
+
+fun main() {
+    val input = readResource("day11/input").trim()
+    var stones = input.split(" ").map { it.toLong() }
+
+
+    val cache = mutableMapOf<Pair<Long, Int>, Long>()
+    println(stones.sumOf { countStones(it, 75, cache) })
+}
