ecd24585789a4a71db4aca2f96241619be800eea
GiantTree <GiantTreeLP@users.noreply.github.com>
2024-12-21 12:54:31 +0100

    Improve the solution for day 6. There is no need to check every spot, just the visited cells of the first run/part. There is also no need to build the adjacency list for each position, just the actual turns taken.

--- src/main/kotlin/year2024/day06.kt
+++ src/main/kotlin/year2024/day06.kt
@@ -24,15 +24,8 @@ class Day06 : AOCSolution {
             .let { (x, y, _) -> return State(Position(x, y), Direction.UP) }
     }
 
-    override fun part1(inputFile: String): String {
-        val map = readResourceLines(inputFile)
-            .map { it.toList() }
-            .toGrid()
-
-        // Find the guard's starting position
-        // The direction is always north
-        val state = findGuardPosition(map)
-
+    private fun simulateGuard(startState: State, map: Grid<Char>) {
+        val state = startState.copy()
         while (state.position in map.bounds) {
             val nextState = state.copy().apply(State::moveForward)
 
@@ -54,6 +47,18 @@ class Day06 : AOCSolution {
                 }
             }
         }
+    }
+
+    override fun part1(inputFile: String): String {
+        val map = readResourceLines(inputFile)
+            .map { it.toList() }
+            .toGrid()
+
+        // Find the guard's starting position
+        // The direction is always north
+        val state = findGuardPosition(map)
+
+        simulateGuard(state, map)
 
         val result = map.count { (_, _, c) -> c == VISITED }
 
@@ -65,65 +70,69 @@ class Day06 : AOCSolution {
             .map { it.toList() }
             .toGrid()
 
+        // Find the guard's starting position
+        // The direction is always north
+        val startState = findGuardPosition(originalMap)
+
+        // Fill the map with the guard's path
+        simulateGuard(startState, originalMap)
+        // Restore the guard at the starting position
+        originalMap[startState.position] = GUARD
+
         val cycles = mutableListOf<Set<Vertex<State>>>()
 
         // Try putting an obstruction on every free cell
         // This approach is not efficient, but it works
-        originalMap.forEach { (x, y, _) ->
-            if (originalMap[x, y] != FREE) {
-                return@forEach
-            }
-            val map = originalMap.copy()
-            map[x, y] = OBSTRUCTION_NEW
+        originalMap
+            // Put an obstruction on each cell the guard has visited
+            .filter { (_, _, cell) -> cell == VISITED }
+            .forEach { (x, y, _) ->
+                val map = originalMap.copy()
+                map[x, y] = OBSTRUCTION_NEW
 
-            // Find the guard's starting position
-            // The direction is always north
-            val startState = findGuardPosition(map)
-            val state = startState.copy()
+                val state = startState.copy()
 
-            val adjacencyList = AdjacencyList<State>()
-            val vertices = Grid(map.width, map.height) { (x, y) ->
-                buildMap {
-                    Direction.ALL.forEach { direction: Direction ->
-                        put(direction, adjacencyList.createVertex(State(Position(x, y), direction)))
-                    }
+                val adjacencyList = AdjacencyList<State>()
+                val vertices = Grid(map.width, map.height) { (x, y) ->
+                    mutableMapOf<Direction, Vertex<State>>()
                 }
-            }
-            val startVertex = vertices[startState.position][startState.direction]!!
+                vertices[startState.position][startState.direction] = adjacencyList.createVertex(startState)
+                val startVertex = vertices[startState.position][startState.direction]!!
 
-            var lastTurn = vertices[state.position][state.direction]!!
+                var lastTurn = startVertex
 
-            // We either encounter a cycle or we exit the map
-            while (state.position in map.bounds) {
-                val nextState = state.copy().apply(State::moveForward)
+                // We either encounter a cycle or we exit the map
+                while (state.position in map.bounds) {
+                    val nextState = state.copy().apply(State::moveForward)
 
-                when (map.getOrNull(nextState.position)) {
-                    // Free
-                    FREE, VISITED, GUARD -> {
-                        state.moveForward()
-                    }
-                    // Obstruction
-                    OBSTRUCTION_OLD, OBSTRUCTION_NEW -> {
-                        state.turnRight()
-                        val v = vertices[state.position][state.direction]!!
-                        adjacencyList.addDirectedEdge(lastTurn, v)
-                        lastTurn = v
+                    when (map.getOrNull(nextState.position)) {
+                        // Free
+                        FREE, VISITED, GUARD -> {
+                            state.moveForward()
+                        }
+                        // Obstruction
+                        OBSTRUCTION_OLD, OBSTRUCTION_NEW -> {
+                            state.turnRight()
+                            val v = vertices[state.position].getOrPut(state.direction) {
+                                adjacencyList.createVertex(state.copy())
+                            }
+                            adjacencyList.addDirectedEdge(lastTurn, v)
+                            lastTurn = v
 
-                        // Check for circular path
-                        val cycle = adjacencyList.getCycle(startVertex)
-                        if (cycle != null) {
-                            cycles.add(cycle)
+                            // Check for circular path
+                            val cycle = adjacencyList.getCycle(startVertex)
+                            if (cycle != null) {
+                                cycles.add(cycle)
+                                break
+                            }
+                        }
+
+                        null -> {
                             break
                         }
                     }
-
-                    null -> {
-                        state.moveForward()
-                        break
-                    }
                 }
             }
-        }
 
         return cycles.size.toString()
     }
