97604ffce831789550eb81f45efd97683131293d
GiantTree <GiantTreeLP@users.noreply.github.com>
2024-12-27 21:51:44 +0100

    Improve the solution for day 1.
    For part 1, a reduction to list copies by one less transposition and not creating pairs eagerly reduces execution time measurably.
    
    For part 2 using a Grouping and counting each number in the right list reduces the amount of array scans/counts.

--- src/main/kotlin/year2024/day01.kt
+++ src/main/kotlin/year2024/day01.kt
@@ -2,9 +2,11 @@ package year2024
 
 import com.google.auto.service.AutoService
 import common.AOCSolution
+import common.grid.toGrid
 import common.pairs
 import common.readResourceLines
 import common.transpose
+import java.util.Collections
 import kotlin.math.abs
 
 @AutoService(AOCSolution::class)
@@ -13,25 +15,27 @@ class Day01 : AOCSolution {
     override val day = 1
 
     override fun part1(inputFile: String): String {
-        return readResourceLines(inputFile)
-            .map { it.split(common.spaceSplitRegex) }
-            .map { it.map(String::toInt) }
+        val lists = readResourceLines(inputFile)
+            .map { it.split(common.spaceSplitRegex).map(String::toInt) }
             .transpose()
-            .map { it.sorted() }
-            .transpose()
-            .pairs()
-            .sumOf { (a, b) -> abs(a - b) }
+            .onEach { it.sorted() }
+
+        val (list1, list2) = lists
+
+        return list1.zip(list2) { a, b -> abs(a - b) }.sum()
             .toString()
     }
 
     override fun part2(inputFile: String): String {
         val lists = readResourceLines(inputFile)
-            .map { it.split(common.spaceSplitRegex) }
-            .map { it.map(String::toInt) }
+            .map { it.split(common.spaceSplitRegex).map(String::toInt) }
             .transpose()
+
         val leftList = lists[0]
         val rightList = lists[1]
-        val result = leftList.sumOf { left -> left * rightList.count { it == left } }
+            .groupingBy { it }
+            .eachCount()
+        val result = leftList.sumOf { left -> left * rightList.getOrDefault(left, 0) }
 
         return result.toString()
     }
